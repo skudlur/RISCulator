c22166ea750be03456c6a536bf92fb1372f7a068
Suhas KV <suhaskv0111@gmail.com>
2023-04-19 19:46:05 +0530

    regfile struct added with traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+/* RISCulator */
+
+// Register Struct
+#[derive(Default,Debug)]
+struct Register {
+    regs: [u32; 32],
+}
+
+// Register Struct traits
+impl Register {
+    // Initialize the registers to 0
+    fn new() -> Self {
+        let regs = [0; 32];
+        Self { regs }
+    }
+
+    // Read data from register
+    fn read(&mut self, index: u32) -> u32 {
+        self.regs[index as usize]
+    }
+
+    // Write data to register
+    fn write(&mut self, index: u32, data: u32) {
+        self.regs[index as usize] = data;
+    }
+}
+
+// RISCulator main function
 fn main() {
-    println!("Hello, world!");
+    let mut reg1 = Register::default();
+    println!("{:?}", reg1);
+
+    reg1.write(2,162);
+    println!("{:?}", reg1);
+
+    let temp = reg1.read(2);
+    println!("{}", temp); 
 }
